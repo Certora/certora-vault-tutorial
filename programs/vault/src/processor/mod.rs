41032b8d1ddd0d91df25dd6a75966bc780c27b4e
Arie Gurfinkel <arie@certora.com>
2025-05-13 19:50:52 -0400

    feat: process for slash

--- programs/vault/src/processor/mod.rs
+++ programs/vault/src/processor/mod.rs
@@ -1,6 +1,7 @@
 pub mod deposit;
 pub mod deposit_with_fee;
 pub mod redeem_shares;
+pub mod slash;
 pub mod update_reward;
 
 pub mod spl_token_utils;
@@ -8,5 +9,6 @@ pub mod spl_token_utils;
 pub use deposit::*;
 pub use deposit_with_fee::*;
 pub use redeem_shares::*;
+pub use slash::*;
 pub use spl_token_utils::*;
 pub use update_reward::*;
